a96f8ea813eb1a1e4d0fd0b706cb4200363a5bc3
Siwawes Wongcharoen <siwaoh@gmail.com>
2020-04-30 13:17:29 +0700

    Update mock data

--- models/Claim.js
+++ models/Claim.js
@@ -13,17 +13,17 @@ class Claim {
         return [
             new Claim({
                 claim_number: 'c20200429001', 
-                application_date: 'a20190429001', 
+                application_date: '2016-08-29T09:12:33.001Z', 
                 policy_number: 'p20190429001'
             }),
             new Claim({
                 claim_number: 'c20200429002', 
-                application_date: 'a20190429002', 
+                application_date: '2016-08-29T09:12:33.001Z', 
                 policy_number: 'p20190429002'
             }),
             new Claim({
                 claim_number: 'c20200429003', 
-                application_date: 'a20190429003', 
+                application_date: '2016-08-29T09:12:33.001Z', 
                 policy_number: 'p20190429003'
             })
         ];
